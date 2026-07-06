61e00713fa08be253b56e0807af9a67fa3364e90
Dima Denisenko <1253839+dimdnk@users.noreply.github.com>
2026-07-06 12:25:09 +0300

    feat: define TenancyAutoConfiguration order

--- src/main/java/com/iqkv/foundation/tenancy/config/TenancyAutoConfiguration.java
+++ src/main/java/com/iqkv/foundation/tenancy/config/TenancyAutoConfiguration.java
@@ -22,16 +22,17 @@ import javax.sql.DataSource;
 import com.iqkv.foundation.tenancy.LiquibaseConfigurationProperties;
 import com.iqkv.foundation.tenancy.TenantKeyProvider;
 import com.iqkv.foundation.tenancy.TenantLiquibaseRunner;
+import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 
 /** Auto-configuration for tenancy components. */
-@Configuration
+@AutoConfiguration(after = DataSourceAutoConfiguration.class)
 @EnableConfigurationProperties(LiquibaseConfigurationProperties.class)
 public class TenancyAutoConfiguration {
 
